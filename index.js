02b979b887fec713491114b96368b563f9b3cafd
Shagun Singh <singhshagun05072003@gmail.com>
2024-06-23 13:29:13 +0530

    Initial commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const socketIO = require("socket.io");
 
 const app=express();
-const port= 4500|| process.env.PORT ;
+const port=4500|| process.env.PORT ;
 
 
 const users=[{}];
@@ -18,9 +18,8 @@ const server=http.createServer(app);
 const io=socketIO(server);
 
 io.on("connection",(socket)=>{
-    console.log("New Connection");
- 
-    socket.on('joined',({user})=>{
+console.log("New Connection");
+ socket.on('joined',({user})=>{
           users[socket.id]=user;
           console.log(`${user} has joined `);
           socket.broadcast.emit('userJoined',{user:"Admin",message:`${users[socket.id]} has  joined`});
