762175218202c280910e4786a300b14e822a4cad
Shagun Singh <singhshagun05072003@gmail.com>
2024-06-23 17:02:04 +0530

    first commit

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
-const express = require("express");
+const http=require("http");
+const express =require("express");
 const cors = require("cors");
-const { createServer } = require("http");
 const socketIO = require("socket.io");
 
-const app = express();
-const port = process.env.PORT || 4500; // Use environment variable PORT or default to 4500
+const app=express();
+const port=4500|| process.env.PORT ;
 
-const users = {};
 
-app.use(cors({
-    origin: "*", // Adjust this for your frontend URL in production
-    methods: ["GET", "POST"],
-}));
+const users=[{}];
 
-app.get("/", (req, res) => {
-    res.send("HELLO, IT'S WORKING");
+app.use(cors());
+app.get("/",(req,res)=>{
+    res.send("HELL ITS WORKING");
+})
+const server=http.createServer(app);
+
+const io=socketIO(server);
+
+io.on("connection",(socket)=>{
+console.log("New Connection");
+ socket.on('joined',({user})=>{
+          users[socket.id]=user;
+          console.log(`${user} has joined `);
+          socket.broadcast.emit('userJoined',{user:"Admin",message:`${users[socket.id]} has  joined`});
+          socket.emit('welcome',{user:"Admin",message:`Welcome to the chat ${users[socket.id]}`});
+         })
+      socket.on('message',({message,id})=>
+    {
+          io.emit('sendmessage',{user:users[id],message,id})
+    })
+    socket.on('disconnect',()=>
+    {
+        socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]} has  left`})
+         console.log('User left')
+    })
 });
 
-const server = createServer(app);
 
-const io = socketIO(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-    }
-});
-
-io.on("connection", (socket) => {
-    console.log("New Connection");
-
-    socket.on('joined', ({ user }) => {
-        users[socket.id] = user;
-        console.log(`${user} has joined`);
-        socket.broadcast.emit('userJoined', { user: "Admin", message: `${users[socket.id]} has joined` });
-        socket.emit('welcome', { user: "Admin", message: `Welcome to the chat ${users[socket.id]}` });
-    });
-
-    socket.on('message', ({ message, id }) => {
-        io.emit('sendMessage', { user: users[id], message, id });
-    });
-
-    socket.on('disconnect', () => {
-        if (users[socket.id]) {
-            socket.broadcast.emit('leave', { user: "Admin", message: `${users[socket.id]} has left` });
-            console.log(`${users[socket.id]} has left`);
-            delete users[socket.id];
-        }
-    });
-});
-
-// Export server to be used in Vercel
-module.exports = server;
+server.listen(port,()=>{
+    console.log(`Working`);
+}) 
